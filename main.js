c6ce8b65f5964f265e30af8d73a4ad3427389667
darmaocean1250 <darmaocean1250@gmail.com>
2026-05-08 05:04:32 +0000

    fix: Disqus shortname 수정 및 인라인 스크립트 적용 (bong-6)

--- main.js
+++ main.js
@@ -64,19 +64,3 @@ generateBtn.addEventListener('click', displayNumbers);
 
 // Initial generation on load
 window.onload = displayNumbers;
-
-/**
- *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
- *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
- */
-var disqus_config = function () {
-  this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-
-(function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://lotto-generator-demo.disqus.com/embed.js'; // Replace 'lotto-generator-demo' with your shortname
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
